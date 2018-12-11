9356e8d13ce3da7c0ffec2805bab08b0d162ffbc
arunsaraswat <arun@mindpropel.com>
2018-12-10 23:08:36 -0600

    Adding mappings from Link to Comment

--- src/main/java/com/mindpropel/springit/domain/Comment.java
+++ src/main/java/com/mindpropel/springit/domain/Comment.java
@@ -7,6 +7,7 @@ import lombok.NonNull;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Data
@@ -20,5 +21,6 @@ public class Comment {
     private String body;
 
     // link
-
+    @ManyToOne
+    private Link link;
 }
