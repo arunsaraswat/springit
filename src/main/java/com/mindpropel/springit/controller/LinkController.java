20bb61144721dfff41d351f28349db2d72191128
arunsaraswat <arun@mindpropel.com>
2018-12-15 21:59:23 -0600

    Added Rest controllers and stubbed out LinkController functions

---
+++ src/main/java/com/mindpropel/springit/controller/LinkController.java
@@ -0,0 +1,47 @@
+package com.mindpropel.springit.controller;
+
+import com.mindpropel.springit.domain.Link;
+import com.mindpropel.springit.repository.LinkRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/links")
+public class LinkController {
+
+    private LinkRepository linkRepository;
+
+    public LinkController(LinkRepository linkRepository) {
+        this.linkRepository = linkRepository;
+    }
+
+    //list
+    @GetMapping("/")
+    public List<Link> list() {
+        return linkRepository.findAll();
+    }
+
+    // CRUD
+    @PostMapping("/create")
+    public Link create(@ModelAttribute Link link) {
+        return linkRepository.save(link);
+    }
+
+    // /links/2
+    @GetMapping("/{id}")
+    public Optional<Link> read(@PathVariable  long id) {
+        return linkRepository.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Link update(@PathVariable Long id, @ModelAttribute Link link) {
+        return linkRepository.save(link);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        linkRepository.deleteById(id);
+    }
+}
